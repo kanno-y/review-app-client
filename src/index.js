f4805ad01a4a15f1d958bb85d0dbaa40f48aa308
kanno-y <kanno0521@gmail.com>
2022-07-14 04:52:17 +0900

    add:  認証機能

--- src/index.js
+++ src/index.js
@@ -1,7 +1,17 @@
 import "bulma/css/bulma.css";
 import { createRoot } from "react-dom/client";
 import { App } from "./App.js";
+import { Auth0Provider } from "@auth0/auth0-react";
 
 const container = document.querySelector("#content");
 const root = createRoot(container);
-root.render(<App />);
+root.render(
+  <Auth0Provider
+    domain={process.env.REACT_APP_AUTH0_DOMAIN}
+    clientId={process.env.REACT_APP_AUTH0_CLIENT_ID}
+    redirectUri={window.location.origin}
+  >
+    <App />
+  </Auth0Provider>,
+  document.querySelector("#content")
+);
